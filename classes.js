fd3b3c347f9dfd57bcbb4cbbfcf217c360ed3020
Christian Inglis <ccinglis@my.waketech.edu>
2026-03-31 14:00:25 -0400

    audio

--- classes.js
+++ classes.js
@@ -2,6 +2,7 @@ const image = document.getElementById("image")
 const text1 = document.getElementById("text1")
 const text2 = document.getElementById("text2")
 const buttonsDiv = document.getElementById("buttons")
+let music = new Audio();
 
 class Scene {
     constructor(image_path, text1, text2)
@@ -28,7 +29,33 @@ class Scene {
             let newButton = document.createElement('button');
             newButton.innerText = "Option " + (i+1);
             newButton.addEventListener('click', () => {buttonsDiv.innerHTML = ""; this.children[i].renderScene()});            buttonsDiv.appendChild(newButton);
+            newButton.addEventListener('click', () => this.exitScene());
+            buttonsDiv.appendChild(newButton);
         }
+    }
+
+    exitScene()
+    {
+
+    }
+}
+
+class MusicScene extends Scene {
+    constructor(image_path, text1, text2)
+    {
+        super(image_path, text1, text2);
+        this.audio_path = audio_path;
+    }
+
+    renderScene() {
+        super.renderScene();
+        music = new Audio(this.audio_path);
+        music.play();
+    }
+    exitScene()
+    {
+        super.exitScene();
+        music   
     }
 }
 
@@ -38,9 +65,10 @@ let introScene = new Scene("tung.jpg",
 let sceneA = new Scene("ballerina.jpg",
     "This is ballerina cappuccina",
     "mimimimi");
-let sceneB = new Scene("bombardino.jpg",
+let sceneB = new MusicScene("bombardino.jpg",
     "This is bombardino crocodillo",
-    "BOMBARDOOOO!");
+    "BOMBARDOOOO!",
+    "Investigations.mp3");
 
 introScene.setChildren([sceneA, sceneB]);
 sceneA.setChildren([introScene]);
